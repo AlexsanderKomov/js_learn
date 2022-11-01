5e767eba5b0141e4f4e27a07e3eab8c9d7cb1c80
Alexsander Komov <sasha.komov.93@mail.ru>
2022-11-01 20:20:18 +0300

    made first part home work

--- DOM/homework1/script.js
+++ DOM/homework1/script.js
@@ -1,3 +1,26 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   let timerStart = document.querySelector('.timer__start');
+//   let timerInput = document.querySelector('.timer__input');
+//   let timerCurrent = document.querySelector('.timer__current');
+//   let interval;
+
+//   timerStart.addEventListener('click', function () {
+//     clearInterval(interval);
+//     interval = setInterval(timerWork, 1000);
+
+//     timerCurrent.textContent = timerInput.value;
+//   });
+
+//   function timerWork() {
+//     let countSeconds = parseInt(timerCurrent.textContent);
+//     if (countSeconds > 0) {
+//       countSeconds -= 1;
+//     }
+
+//     return (timerCurrent.textContent = countSeconds);
+//   }
+// });
+
 document.addEventListener('DOMContentLoaded', function () {
   let timerStart = document.querySelector('.timer__start');
   let timerInput = document.querySelector('.timer__input');
@@ -6,17 +29,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
   timerStart.addEventListener('click', function () {
     clearInterval(interval);
-    interval = setInterval(timerWork, 1000);
-
-    timerCurrent.textContent = timerInput.value;
+    if (timerInput.value > 0) {
+      timerCurrent.textContent = timerInput.value;
+      interval = setInterval(timeCounter, 1000);
+    } else {
+      timerCurrent.textContent = 0;
+    }
   });
 
-  function timerWork() {
-    let countSeconds = parseInt(timerCurrent.textContent);
-    if (countSeconds > 0) {
-      countSeconds -= 1;
+  function timeCounter() {
+    if (parseInt(timerCurrent.textContent) > 0) {
+      timerCurrent.textContent -= 1;
+    } else {
+      clearInterval(interval);
+      timerCurrent.textContent = 0;
     }
-
-    return (timerCurrent.textContent = countSeconds);
   }
 });
