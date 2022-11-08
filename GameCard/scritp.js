0945911d6e203f49f4f5093124270d11c35540e7
Alexsander Komov <sasha.komov.93@mail.ru>
2022-11-08 23:30:16 +0300

    made game and add file lesson

--- GameCard/scritp.js
+++ GameCard/scritp.js
@@ -1,11 +1,24 @@
 (function () {
   let arrayItem = [];
-  let arrayNumber = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+  const arrayNumber = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
   let activeNumder = [];
   let countCardSuccess = [];
 
+  const form = document.querySelector('.form');
+  const input = document.querySelector('.form__input');
+  let btn = document.querySelector('.form__btn');
+  const gameList = document.querySelector('.list');
+
+  input.addEventListener('input', function () {
+    if (!input.value) {
+      btn.disabled = true;
+    } else {
+      btn.disabled = false;
+    }
+  });
+
   function createGameRepeat() {
-    let buttonRepeat = document.createElement('button');
+    const buttonRepeat = document.createElement('button');
     buttonRepeat.classList.add('form-btn');
     buttonRepeat.textContent = 'Сыграть еще';
     buttonRepeat.className = 'button-repeat';
@@ -14,123 +27,29 @@
     return buttonRepeat;
   }
 
-  // создаем и возвращаем заголовок приложения
-  function createAppTitle() {
-    let appTitle = document.createElement('h1');
-    appTitle.innerHTML = 'Игра в пары';
-    appTitle.style.marginBottom = '15px';
-
-    return appTitle;
-  }
-
-  // создаем и возвращаем форму начала игры
-  function createForm() {
-    let form = document.createElement('form');
-    let input = document.createElement('input');
-    let btn = document.createElement('button');
-
-    form.id = 'form';
-    form.classList.add('form');
-    input.classList.add('form__input');
-    input.id = 'input';
-    input.placeholder = 'Введите количество карточек по горизонтали/вертикали';
-    btn.classList.add('form__btn');
-    btn.textContent = 'Начать';
-
-    btn.disabled = true;
-
-    form.append(input);
-    form.append(btn);
-
-    input.addEventListener('input', function () {
-      if (input.value) {
-        btn.disabled = false;
-      } else {
-        btn.disabled = true;
-      }
-    });
-
-    return {
-      form,
-      input,
-      btn,
-    };
-  }
-
   // создаем предупреждение если ввели другое число
   function creareError() {
-    let error = document.createElement('h3');
+    const error = document.createElement('h3');
     error.innerHTML = 'Вы не попали в диапазон поэтому 4 в ряд по умолчанию';
     error.style.display = 'none';
 
     return error;
   }
 
-  // создаем и возвращаем список элементов
-  function createGameList() {
-    let cardList = document.createElement('ul');
-    cardList.classList.add('list');
-
-    return cardList;
-  }
-
   // создаем и возвращаем поле для игры
   function createGameField(number) {
     let cardItem = document.createElement('li');
-    let cardText = document.createElement('span');
 
     cardItem.classList.add('item');
-    cardText.classList.add('item__text');
 
-    cardText.textContent = number;
+    cardItem.textContent = number;
 
-    cardItem.addEventListener('click', () => {
-      if (cardItem.classList == 'item' && cardText.classList == 'item__text') {
-        cardItem.classList.add('item-active');
-        cardText.classList.add('item__text-active');
-        activeNumder.push(cardItem);
-        activeNumder.push(cardText);
-      }
-
-      // проверяем значения на совпадение и убираем класс или оставляем
-      if (
-        activeNumder.length == 4 &&
-        activeNumder[1].textContent === activeNumder[3].textContent
-      ) {
-        countCardSuccess.push(activeNumder[0]);
-        countCardSuccess.push(activeNumder[2]);
-
-        activeNumder = [];
-      }
-      if (
-        activeNumder.length == 4 &&
-        activeNumder[1].textContent !== activeNumder[3].textContent
-      ) {
-        setTimeout(() => {
-          activeNumder[0].classList.remove('item-active');
-          activeNumder[1].classList.remove('item__text-active');
-          activeNumder[2].classList.remove('item-active');
-          activeNumder[3].classList.remove('item__text-active');
-          activeNumder = [];
-        }, 1000);
-      }
-
-      if (arrayItem.length === countCardSuccess.length) {
-        document.querySelector('.button-repeat').style.display = 'block';
-        document.querySelector('.list').style.marginBottom = '20px';
-        arrayItem = [];
-        activeNumder = [];
-        setTimeout(() => {
-          alert('Победа');
-        }, 700);
-      }
+    // тут видит
+    cardItem.addEventListener('click', function () {
+      cardItem.classList.add('item-active');
+      activeNumder.push(cardItem);
+      console.log(activeNumder);
     });
-
-    if (cardText.textContent === 'А тут пусто') {
-      cardText.style.fontSize = '16px';
-    }
-
-    cardItem.append(cardText);
 
     return cardItem;
   }
@@ -145,74 +64,148 @@
   }
 
   function createGameApp(container) {
-    let appTitle = createAppTitle();
-    let gameForm = createForm();
-    let gameList = createGameList();
-    let gameErrorValue = creareError();
-    let gameRepeat = createGameRepeat();
+    let gameItem = createGameField();
+    const gameErrorValue = creareError();
+    const gameRepeat = createGameRepeat();
 
-    container.append(appTitle);
-    container.append(gameForm.form);
-    container.append(gameErrorValue);
-    container.append(gameList);
-    container.append(gameRepeat);
-
-    gameForm.form.addEventListener('submit', function (e) {
+    form.addEventListener('submit', function (e) {
       e.preventDefault();
 
-      gameForm.form.remove();
-      container.append(gameRepeat);
+      form.remove();
+
       // игнорируем создание элемента, если пользователь ничего не ввел в поле
-      if (!gameForm.input.value || gameForm.input.value == /[0-9]/) {
+      if (!input.value) {
         return;
       }
 
-      if (gameForm.input.value ** 2 === 16 || gameForm.input.value % 2 === 1) {
+      // корректное заполнение значением карточек если ввели 4
+      if (input.value > 10 || input.value <= 4) {
+        fyShuffle(arrayNumber, input.value ** 2);
+
+        for (let i = 0; i < input.value ** 2; i++) {
+          arrayItem.push(createGameField(arrayNumber[i]));
+          gameList.append(createGameField(arrayNumber[i]));
+        }
+      }
+
+      // корректное заполнение значением карточек если ввели 6
+      if (input.value % 2 === 0 && input.value == 6) {
+        let arrayNumberValueSix = arrayNumber
+          .concat(arrayNumber)
+          .concat(arrayNumber)
+          .slice(0, 36);
+
+        fyShuffle(arrayNumberValueSix, input.value ** 2);
+
+        for (let i = 0; i < arrayNumberValueSix.length; i++) {
+          arrayItem.push(createGameField(arrayNumberValueSix[i]));
+          gameList.append(createGameField(arrayNumberValueSix[i]));
+        }
+      }
+
+      // корректное заполнение значением карточек если ввели 8
+      if (input.value % 2 === 0 && input.value == 8) {
+        let arrayNumberValueEight = arrayNumber
+          .concat(arrayNumber)
+          .concat(arrayNumber)
+          .concat(arrayNumber);
+
+        fyShuffle(arrayNumberValueEight, input.value ** 2);
+
+        for (let i = 0; i < arrayNumberValueEight.length; i++) {
+          arrayItem.push(createGameField(arrayNumberValueEight[i]));
+          gameList.append(createGameField(arrayNumberValueEight[i]));
+        }
+      }
+
+      // корректное заполнение значением карточек если ввели 10
+      if (input.value % 2 === 0 && input.value == 10) {
+        let arrayNumberValueTen = arrayNumber
+          .concat(arrayNumber)
+          .concat(arrayNumber)
+          .concat(arrayNumber)
+          .concat(arrayNumber)
+          .concat(arrayNumber)
+          .slice(0, 100);
+
+        fyShuffle(arrayNumberValueTen, input.value ** 2);
+
+        for (let i = 0; i < arrayNumberValueTen.length; i++) {
+          arrayItem.push(createGameField(arrayNumberValueTen[i]));
+          gameList.append(createGameField(arrayNumberValueTen[i]));
+        }
+      }
+
+      // адаптация списка и карточек под стандартное кол-во
+      if (input.value ** 2 === 16 || input.value % 2 === 1) {
         gameList.style.width = '645px';
       }
-
-      if (gameForm.input.value ** 2 === 36) {
+      // адаптация списка и карточек под 6 карточек в ряд
+      if (input.value ** 2 === 36) {
         gameList.style.width = '990px';
       }
-
-      if (gameForm.input.value ** 2 === 64) {
+      // адаптация списка и карточек под 8 карточек в ряд
+      if (input.value ** 2 === 64) {
         gameList.style.width = '100%';
+        document
+          .querySelectorAll('.item')
+          .forEach(
+            (el) => ((el.style.width = '132px'), (el.style.height = '174px'))
+          );
       }
-
-      let numder;
-
-      if (gameForm.input.value % 2 === 0 && gameForm.input.value <= 10) {
-        fyShuffle(arrayNumber, gameForm.input.value ** 2);
-
-        for (let i = 0; i < gameForm.input.value ** 2; i++) {
-          numder = arrayNumber[i];
-          if (numder === '' || numder === undefined) {
-            numder = 'А тут пусто';
-          }
-          arrayItem.push(createGameField(numder));
-          gameList.append(createGameField(numder));
-        }
-      } else {
-        fyShuffle(arrayNumber);
-
-        for (let i = 0; i < 16; i++) {
-          gameErrorValue.style.display = 'block';
-          numder = arrayNumber[i];
-          arrayItem.push(createGameField(numder));
-          gameList.append(createGameField(numder));
-        }
+      // адаптация списка и карточек под 10 карточек в ряд
+      if (input.value ** 2 === 100) {
+        gameList.style.width = '100%';
+        document
+          .querySelectorAll('.item')
+          .forEach(
+            (el) => ((el.style.width = '103px'), (el.style.height = '136px'))
+          );
       }
     });
 
-    gameRepeat.addEventListener('click', () => {
-      let allItem = document.querySelectorAll('.item');
-      for (let item of allItem) {
-        item.remove();
-      }
-      container.append(gameForm.form);
-      gameRepeat.remove();
-      console.log(arrayItem);
+    // ТУТ не видит клика хотя передаю функцию, которая возвращает item
+    gameItem.addEventListener('click', () => {
+      console.log(345);
     });
+
+    // if (arrayItem.length === countCardSuccess.length) {
+    //   document.querySelector('.button-repeat').style.display = 'block';
+    //   document.querySelector('.list').style.marginBottom = '20px';
+    //   arrayItem = [];
+    //   countCardSuccess = [];
+    //   setTimeout(() => {
+    //     alert('Победа');
+    //   }, 700);
+    // }
+
+    console.log(activeNumder);
+    // проверяем значения на совпадение и убираем класс или оставляем
+    if (activeNumder.length == 2) {
+      // countCardSuccess.push(activeNumder[0]);
+      // countCardSuccess.push(activeNumder[1]);
+      console.log(activeNumder);
+      activeNumder = [];
+    }
+    if (
+      activeNumder.length == 2
+      // activeNumder[0].textContent !== activeNumder[1].textContent
+    ) {
+      setTimeout(() => {
+        activeNumder[0].classList.remove('item-active');
+        activeNumder[1].classList.remove('item-active');
+        activeNumder = [];
+      }, 200);
+    }
+
+    // кнопка повтора
+    // gameRepeat.addEventListener('click', () => {
+    //   arrayItem = [];
+    //   countCardSuccess = [];
+    //   container.append(gameForm.form);
+    //   gameRepeat.remove();
+    //   console.log(countCardSuccess);
+    // });
   }
 
   window.createGameApp = createGameApp;
